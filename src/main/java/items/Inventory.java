ca812910774d0d94e4cda87a29c2a6c244cae366
Michael Nimitz <mnimi001@odu.edu>
2025-02-12 17:07:55 -0500

    added 3 methods

--- src/main/java/items/Inventory.java
+++ src/main/java/items/Inventory.java
@@ -31,6 +31,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
     public static void mergeStacks(ItemStack lhs, ItemStack rhs)
     {
         // Refer to the notes from Assignment 1
+        lhs.addItems(rhs.size());
     }
 
     /**
@@ -95,7 +96,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
     public boolean isFull()
     {
         // Replace the next line
-        return false;
+        return this.slots.size() == 0;
     }
 
     /**
@@ -131,6 +132,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
     public void addItemStackNoCheck(ItemStack toAdd)
     {
         // Add the missing (one) line by using `this.slots.add(????)`
+        this.slots.add(toAdd);
     }
 
     /**
